8fb5550da528d4e1ee7e42526f01054c8a1f7862
王鹏龙 <1368681085@qq.com>
2019-02-26 19:20:14 +0800

    BackTracking

---
+++ src/ADT/TreeNode.java
@@ -0,0 +1,10 @@
+package ADT;
+
+/**
+ * Create by 王鹏龙 on 2019-02-23 18:29
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode(int x) {val = x;}
+}
---
+++ src/BackTracking/N皇后.java
@@ -0,0 +1,10 @@
+package BackTracking;
+
+/**
+ * Create by 王鹏龙 on 2019-02-23 19:09
+ */
+public class N皇后 {
+    public static void mian(String[] args){
+        StringBuilder sb = new StringBuilder();
+    }
+}
